8ee98b83af9acbb4b9d840cffd7b8bc2bf510969
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2022-12-19 16:02:00 -0500

    Part 2 day 2 completed

--- day2/aoc_d2Part2.py
+++ day2/aoc_d2Part2.py
@@ -15,8 +15,40 @@ scoreCounter = 0
 with open("day2\d2_input.txt", "r") as f:
     for line in f:
         
-        # I need to Lose
+        # I need to lose
         if rpsValues[line.split()[1]] == 1:
-            scoreCounter += rpsValues[line.split()[1]] + 3
+            # Op has rock
+            if rpsValues[line.split()[0]] == 1:
+                scoreCounter += rpsValues["C"]
+            # Op has paper
+            elif rpsValues[line.split()[0]] == 2:
+                scoreCounter += rpsValues["A"]
+            # Op has scissors
+            elif rpsValues[line.split()[0]] == 3:
+                scoreCounter += rpsValues["B"]
+
+        # I need to draw
+        elif rpsValues[line.split()[1]] == 2:
+            # Op has rock
+            if rpsValues[line.split()[0]] == 1:
+                scoreCounter += rpsValues[line.split()[0]] + 3
+            # Op has paper
+            elif rpsValues[line.split()[0]] == 2:
+                scoreCounter += rpsValues[line.split()[0]] + 3
+            # Op has scissors
+            elif rpsValues[line.split()[0]] == 3:
+                scoreCounter += rpsValues[line.split()[0]] + 3
+
+        # I need to win
+        elif rpsValues[line.split()[1]] == 3:
+            # Op has rock
+            if rpsValues[line.split()[0]] == 1:
+                scoreCounter += rpsValues["B"] + 6
+            # Op has paper
+            elif rpsValues[line.split()[0]] == 2:
+                scoreCounter += rpsValues["C"] + 6
+            # Op has scissors
+            elif rpsValues[line.split()[0]] == 3:
+                scoreCounter += rpsValues["A"] + 6
 
     print(scoreCounter)
